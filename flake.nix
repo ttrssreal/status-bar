bc345b7b0c140b5edee5503a57705e327d59418d
Jess <jess@jessie.cafe>
2024-09-22 08:51:18 +1200

    Add a flake output

--- flake.nix
+++ flake.nix
@@ -11,7 +11,25 @@
         pkgs = import nixpkgs {
           inherit system overlays;
         };
+        runtimeDeps = pkgs.lib.makeBinPath (with pkgs; [
+          alsa-utils
+          networkmanager
+        ]);
       in {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "status_bar";
+          version = "0.1";
+
+          src = ./.;
+
+          nativeBuildInputs = with pkgs; [ makeWrapper ];
+
+          cargoHash = "sha256-mtC0/KKfc/Yz69PoWElAxTi3qQnufCLwDQm4wXOVnII=";
+
+          postInstall = ''
+            wrapProgram $out/bin/status_bar --prefix PATH : ${runtimeDeps}
+          '';
+        };
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             rust-bin.stable.latest.default
